14db59d4d79c628e8f67ce89f24d1314031fc372
Matt (student test) <matt.rudge@codeinstitute.net>
2024-10-08 09:59:13 +0000

    Updated lesson 05

--- 05_building_views_templates/tasks/views.py
+++ 05_building_views_templates/tasks/views.py
@@ -3,12 +3,12 @@ from .models import Task
 
 def home(request):
     """
-    A view to display the tasks to do and the completed tasks
+    A view to display tasks to do and completed tasks
     with the tasks due soonest at the top
     """
 
     to_do_tasks = Task.objects.filter(completed=False).order_by('due_date')
-    done_tasks = Task.objects.filter(completed=True).order_by('-due_date')
+    done_tasks = Task.objects.filter(completed=True).order_by('due_date')
 
     context = {
         'to_do_tasks': to_do_tasks,
@@ -16,5 +16,3 @@ def home(request):
     }
 
     return render(request, 'tasks/index.html', context)
-
-# Create your views here.
