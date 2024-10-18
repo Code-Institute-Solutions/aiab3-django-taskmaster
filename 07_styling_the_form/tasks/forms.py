cc2a79d86c0291d4f6528b916e54418fcefecd47
Matt (student test) <matt.rudge@codeinstitute.net>
2024-10-18 16:12:17 +0000

    Style form

--- 07_styling_the_form/tasks/forms.py
+++ 07_styling_the_form/tasks/forms.py
@@ -9,7 +9,5 @@ class TaskForm(forms.ModelForm):
         fields = ['title', 'due_date', 'category']
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Task Title'}),
-            'due_date': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Task Due Date'}),
-            'category': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Task Category'}),
+            'due_date': forms.DateInput(attrs={'type': 'date'})
         }
