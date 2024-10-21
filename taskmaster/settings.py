4085ddca07124532e43b1ad5cda303dff3c7f68d
Matt (student test) <matt.rudge@codeinstitute.net>
2024-10-21 11:17:07 +0000

    Deployment

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*.herokuapp.com"]
+ALLOWED_HOSTS = ["ci-taskmaster-final-dff252a883f7.herokuapp.com"]
 CSRF_TRUSTED_ORIGINS = ["//*.herokuapp.com"]
 
 
